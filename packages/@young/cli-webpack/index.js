276ee36a1763ca41648bed85d71e052bd627c6bb
李阳(10014141) <liy-cb@glodon.com>
2020-07-16 10:34:13 +0800

    build: update dir

--- packages/@young/cli-webpack/index.js
+++ packages/@young/cli-webpack/index.js
@@ -2,6 +2,8 @@ const webpack = require( 'webpack' );
 const merge = require( 'webpack-merge' )
 const Config = require( 'webpack-chain' );
 
-module.exports = ( api, options ) => {
-    
+module.exports = ( options ) => {
+    const config = new Config();
+
+    return config.toConfig();
 }
